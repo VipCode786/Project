97224c5a0da62860d645d941049e96eca059cbcc
Abhishek Jha <66509743+abhishek5670@users.noreply.github.com>
2022-10-13 14:24:51 +0530

    Update sales.js

--- sales/sales.js
+++ sales/sales.js
@@ -1,12 +1,19 @@
 import mongoose from "mongoose";
-
+import mongoosesequence   from 'mongoose-sequence';
+import testing from "./testing.js";
+//const AutoIncrement   = mongoosesequence(mongoose);
+//const AutoIncrement = require('mongoose-sequence')(mongoose);
+//import testing from '../sales/testing'
 
 
 const supplierSchema = new mongoose.Schema({
+    id: { type: String,
+         required:true},
     Name: { type: String, required: true },
 
     Email: { type: String, required: true},
         //unique: true},
+
 
      Phone: {
         type: String,
@@ -14,7 +21,16 @@ const supplierSchema = new mongoose.Schema({
         //unique: true
      },
 
+    //  count:{
+    //     require:true
+    //  },
 
+    //  id1:{
+    //     required:true,
+    //  },
+    //  seq:{
+    //     type:Number
+    //  },
 
     BillingAddress :{
         billingAddress: {type: String, required: true },
@@ -32,10 +48,35 @@ const supplierSchema = new mongoose.Schema({
         State: {type: String, required: true },
         Country: {type: String, required: true },
         Pincode : {type: String, required: true }
-    }
+    },
+   
 })
 
+//supplierSchema.plugin(AutoIncrement,{inc_field:('id'),prefix:'SUP0_'})
+//console.log(b)
 
+supplierSchema.pre("save", function (next) {
+    let doc = this;
+    testing.getSequenceNextValue("Supplier_id").
+    then(counter => {
+        console.log("asdasd", counter);
+        if(!counter) {
+            testing.insertCounter("Supplier_id")
+            .then(counter => {
+                console.log(doc)
+                doc.id =`SUP${counter}`;
+                
+                console.log(doc.id)
+                next();
+            })
+            .catch(error => next(error))
+        } else {
+            doc.id = `SUP${counter}`;
+            next();
+        }
+    })
+    .catch(error => next(error))
+});
 
 const Supplier = mongoose.model('Supplier', supplierSchema);
-export default Supplier;
+export default Supplier
